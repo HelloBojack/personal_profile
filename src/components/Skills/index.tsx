464b317578425a7b87a68449a1b2ea3a1f8eb839
bojack <xlk970115@gmail.com>
2022-08-29 22:34:02 +0800

    fix skills

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -4,7 +4,7 @@ import Carousel, { Alignment } from "nuka-carousel";
 export const Skills = () => {
   return (
     <section className="container mx-auto text-center pb-[60px]" id="skills">
-      <div className="bg-[#121212] rounded-[64px] flex flex-wrap items-center shadow-fuchsia-900 shadow-lg -mt-[60px] px-[50px] py-[60px]">
+      <div className="bg-[#121212] rounded-[64px] flex flex-wrap items-center shadow-pink shadow-lg -mt-[60px] px-[50px] py-[60px]">
         <div className="w-full mb-[75px]">
           <h2 className="text-[45px] font-bold">Skills</h2>
           <p className="text-[18px] tracking-[0.8px] leading-[1.5em] mt-[14px] text-[#B8B8B8]">
